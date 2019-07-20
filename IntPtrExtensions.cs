a78b765c5c83afed37f8a95e185ed4031cc62a66
Константин Дьяченко <konard@me.com>
2019-07-20 19:17:43 +0300

    CodeFactor issues fix

--- IntPtrExtensions.cs
+++ IntPtrExtensions.cs
@@ -13,10 +13,10 @@ namespace Platform.Unsafe
         public static void SetValue<TElement>(this IntPtr pointer, TElement value) => IntPtrHelpers<TElement>.SetValue(pointer, value);
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static IntPtr GetElement(this IntPtr pointer, int elementSize, int index) => pointer + elementSize * index;
+        public static IntPtr GetElement(this IntPtr pointer, int elementSize, int index) => pointer + (elementSize * index);
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static unsafe IntPtr GetElement(this IntPtr pointer, long elementSize, long index) => new IntPtr((byte*)pointer.ToPointer() + elementSize * index);
+        public static unsafe IntPtr GetElement(this IntPtr pointer, long elementSize, long index) => new IntPtr((byte*)pointer.ToPointer() + (elementSize * index));
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static IntPtr GetElement<TIndex>(this IntPtr pointer, int elementSize, TIndex index) => pointer.GetElement((long)elementSize, (Integer)(Integer<TIndex>)index);
