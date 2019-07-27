c7c77327f91c10acfac7346be1b8e53ebac57e8f
Константин Дьяченко <konard@me.com>
2019-07-27 16:32:25 +0300

    Codacy issue fix.

--- IntPtrHelpers.cs
+++ IntPtrHelpers.cs
@@ -25,7 +25,7 @@ namespace Platform.Unsafe
                 else
                 {
                     emiter.LoadArguments(0);
-                    emiter.Call(typeof(Marshal).GetGenericMethod("PtrToStructure", Types.Get<T>().ToArray(), Types<IntPtr, Type, bool>.List.ToArray())); ;
+                    emiter.Call(typeof(Marshal).GetGenericMethod("PtrToStructure", Types.Get<T>().ToArray(), Types<IntPtr, Type, bool>.List.ToArray()));
                     emiter.Return();
                 }
             });
