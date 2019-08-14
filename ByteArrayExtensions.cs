c24cfc150495138ef34a30728fe602c9e2a8b8fb
Константин Дьяченко <konard@me.com>
2019-08-14 13:13:00 +0300

    ByteArrayExtensions forked from StructureExtensions.

---
+++ ByteArrayExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System.Runtime.InteropServices;
+using Platform.Exceptions;
+using Platform.Collections;
+
+namespace Platform.Unsafe
+{
+    public static class ByteArrayExtensions
+    {
+        public static TTStruct ToStructure<TTStruct>(this byte[] bytes)
+            where TTStruct : struct
+        {
+            Ensure.Always.ArgumentNotEmpty(bytes, nameof(bytes));
+            var structureSize = Structure.SizeOf<TTStruct>();
+            Ensure.Always.ArgumentMeetsCriteria(array => array.Length == structureSize, bytes, nameof(bytes), "Bytes array should be the same length as struct size.");
+            var pointer = Marshal.AllocHGlobal(structureSize);
+            Marshal.Copy(bytes, 0, pointer, structureSize);
+            var structure = Marshal.PtrToStructure<TTStruct>(pointer);
+            Marshal.FreeHGlobal(pointer);
+            return structure;
+        }
+    }
+}
--- StructureExtensions.cs
+++ StructureExtensions.cs
@@ -1,6 +1,4 @@
 ﻿using System.Runtime.InteropServices;
-using Platform.Exceptions;
-using Platform.Collections;
 
 namespace Platform.Unsafe
 {
@@ -16,19 +14,6 @@ namespace Platform.Unsafe
             Marshal.Copy(pointer, bytes, 0, structureSize);
             Marshal.FreeHGlobal(pointer);
             return bytes;
-        }
-
-        public static TTStruct ToStructure<TTStruct>(this byte[] bytes)
-            where TTStruct : struct
-        {
-            Ensure.Always.ArgumentNotEmpty(bytes, nameof(bytes));
-            var structureSize = Structure.SizeOf<TTStruct>();
-            Ensure.Always.ArgumentMeetsCriteria(array => array.Length == structureSize, bytes, nameof(bytes), "Bytes array should be the same length as struct size.");
-            var pointer = Marshal.AllocHGlobal(structureSize);
-            Marshal.Copy(bytes, 0, pointer, structureSize);
-            var structure = Marshal.PtrToStructure<TTStruct>(pointer);
-            Marshal.FreeHGlobal(pointer);
-            return structure;
         }
     }
 }
