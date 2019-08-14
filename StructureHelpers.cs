4aaaa8f7059146a8aa2300a92c3dda4c3a509b78
Константин Дьяченко <konard@me.com>
2019-08-14 13:01:11 +0300

    Comments translated to russian, see tag is now used instead of plain text, link to code updated.

--- StructureHelpers.cs
+++ StructureHelpers.cs
@@ -5,15 +5,30 @@ namespace Platform.Unsafe
     public static class StructureHelpers
     {
         /// <summary>
+        /// <para>
         /// Returns the size of an unmanaged type in bytes.
-        /// This method do this without throwing exceptions for generic types as Marshal.SizeOf[T]() and Marshal.SizeOf(Type type) do.
+        /// This method do this without throwing exceptions for generic types as <see cref="Marshal.SizeOf{T}"/> and <see cref="Marshal.SizeOf(System.Type)"/> do.
+        /// </para>
+        /// <para>
+        /// Возвращает размер неуправляемого типа в байтах.
+        /// Этот метод делает это без выбрасывания исключений для универсальных типов, как это делают<see cref = "Marshal.SizeOf {T}" /> и < see cref = "Marshal.SizeOf (System.Type)" />.
+        /// </para>
         /// </summary>
         /// <remarks>
+        /// <para>
         /// Based on proposed solution at https://stackoverflow.com/a/18167584/710069
         /// For actual differences in .NET code see:
         /// https://github.com/Microsoft/referencesource/blob/f82e13c3820cd04553c21bf6da01262b95d9bd43/mscorlib/system/runtime/interopservices/marshal.cs#L202
-        /// https://github.com/Microsoft/referencesource/blob/f82e13c3820cd04553c21bf6da01262b95d9bd43/mscorlib/system/runtime/interopservices/marshal.cs#L219
+        /// https://github.com/Microsoft/referencesource/blob/f82e13c3820cd04553c21bf6da01262b95d9bd43/mscorlib/system/runtime/interopservices/marshal.cs#L219-L222
         /// Note that this behaviour can be changed in future versions of .NET
+        /// </para>
+        /// <para>
+        /// На основе предложенного решения https://stackoverflow.com/a/18167584/710069
+        /// Фактические различия в коде .NET:
+        /// https://github.com/Microsoft/referencesource/blob/f82e13c3820cd04553c21bf6da01262b95d9bd43/mscorlib/system/runtime/interopservices/marshal.cs#L202
+        /// https://github.com/Microsoft/referencesource/blob/f82e13c3820cd04553c21bf6da01262b95d9bd43/mscorlib/system/runtime/interopservices/marshal.cs#L219-L222
+        /// Обратите внимание, что это поведение может быть изменено в будущих версиях.NET
+        /// </para>
         /// </remarks>
         public static int SizeOf<TStruct>() where TStruct : struct => Marshal.SizeOf(default(TStruct));
     }
