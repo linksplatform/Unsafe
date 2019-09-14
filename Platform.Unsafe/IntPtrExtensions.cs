9bcd584a0412576029bcd64b7debfd4f4c7507d1
Konstantin Dyachenko <drakonard@gmail.com>
2019-09-14 16:30:38 +0300

    Closes #19.

--- Platform.Unsafe/IntPtrExtensions.cs
+++ Platform.Unsafe/IntPtrExtensions.cs
@@ -9,7 +9,7 @@ namespace Platform.Unsafe
     /// <remarks>
     /// Please use System.Runtime.CompilerServices.Unsafe instead.
     /// </remarks>
-    public static class IntPtrExtensions
+    public unsafe static class IntPtrExtensions
     {
         [Obsolete("GetValue method is deprecated, please use System.Runtime.CompilerServices.Unsafe.Read method instead.")]
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
@@ -25,10 +25,13 @@ namespace Platform.Unsafe
 
         [Obsolete("GetElement method is deprecated, please use System.Runtime.CompilerServices.Unsafe.Add method instead.")]
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static unsafe IntPtr GetElement(this IntPtr pointer, long elementSize, long index) => new IntPtr((byte*)pointer.ToPointer() + (elementSize * index));
+        public static IntPtr GetElement(this IntPtr pointer, long elementSize, long index) => new IntPtr((byte*)pointer.ToPointer() + (elementSize * index));
 
         [Obsolete("GetElement method is deprecated, please use System.Runtime.CompilerServices.Unsafe.Add method instead.")]
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static IntPtr GetElement<TIndex>(this IntPtr pointer, int elementSize, TIndex index) => pointer.GetElement((long)elementSize, (Integer)(Integer<TIndex>)index);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void WriteElementValue<TValue>(this IntPtr pointer, long index, TValue value) => System.Runtime.CompilerServices.Unsafe.Write((byte*)pointer + (System.Runtime.CompilerServices.Unsafe.SizeOf<TValue>() * index), value);
     }
 }
