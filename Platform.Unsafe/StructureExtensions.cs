fd8092c947b5f9b3115fc9a1cd29052a4736c3a6
Konstantin Dyachenko <drakonard@gmail.com>
2019-09-14 16:25:58 +0300

    Closes #23. Added CopyTo method with 2 overloads to StructureExtensions.

--- Platform.Unsafe/StructureExtensions.cs
+++ Platform.Unsafe/StructureExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System.Runtime.CompilerServices;
+using Platform.Hardware.Cpu;
 
 #pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
 
@@ -14,9 +15,31 @@ namespace Platform.Unsafe
             var bytes = new byte[structureSize];
             fixed (byte* pointer = bytes)
             {
-                System.Runtime.CompilerServices.Unsafe.Copy(pointer, ref obj);
+                obj.CopyTo(pointer, structureSize);
             }
             return bytes;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void CopyTo<TStruct>(this ref TStruct source, void* destination)
+            where TStruct : struct
+        {
+            var size = System.Runtime.CompilerServices.Unsafe.SizeOf<TStruct>();
+            CopyTo(ref source, destination, size);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void CopyTo<TStruct>(this ref TStruct source, void* destination, int size)
+            where TStruct : struct
+        {
+            if (CacheLine.Size <= size)
+            {
+                System.Runtime.CompilerServices.Unsafe.Copy(destination, ref source);
+            }
+            else
+            {
+                System.Runtime.CompilerServices.Unsafe.CopyBlock(destination, System.Runtime.CompilerServices.Unsafe.AsPointer(ref source), (uint)size);
+            }
         }
     }
 }
