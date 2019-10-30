aa7a658fdc0307433f898b79877fd2e7eabab83a
Константин Дьяченко <konard@me.com>
2019-10-30 23:05:05 +0300

    Benchmarks added.

---
+++ Platform.Unsafe.Benchmarks/MemoryBlockBenchmarks.cs
@@ -0,0 +1,37 @@
+﻿using BenchmarkDotNet.Attributes;
+
+#pragma warning disable CA1822 // Mark members as static
+
+namespace Platform.Unsafe.Benchmarks
+{
+    [SimpleJob]
+    [MemoryDiagnoser]
+    public unsafe class MemoryBlockBenchmarks
+    {
+        [Params(10000, 1000000, 100000000)]
+        public int N { get; set; }
+
+        private static byte[] _array;
+
+        [GlobalSetup]
+        public static void Setup() => _array = new byte[4096 * 1024];
+
+        [Benchmark]
+        public void Zero()
+        {
+            fixed (byte* pointer = _array)
+            {
+                MemoryBlock.Zero(pointer, _array.Length);
+            }
+        }
+
+        [Benchmark]
+        public void ParallelZero()
+        {
+            fixed (byte* pointer = _array)
+            {
+                MemoryBlock.ParallelZero(pointer, _array.Length);
+            }
+        }
+    }
+}
---
+++ Platform.Unsafe.Benchmarks/Program.cs
@@ -0,0 +1,9 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace Platform.Unsafe.Benchmarks
+{
+    static class Program
+    {
+        static void Main() => BenchmarkRunner.Run<MemoryBlockBenchmarks>();
+    }
+}
