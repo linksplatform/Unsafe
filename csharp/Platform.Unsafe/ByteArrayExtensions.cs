89561bd859d953b44b67238f91e93e8847232861
DdeadliestT <93347396+DdeadliestT@users.noreply.github.com>
2023-02-09 22:12:23 +0300

    Update ByteArrayExtensions.cs
    Add comments to the code.

--- csharp/Platform.Unsafe/ByteArrayExtensions.cs
+++ csharp/Platform.Unsafe/ByteArrayExtensions.cs
@@ -8,31 +8,19 @@ using static System.Runtime.CompilerServices.Unsafe;
 namespace Platform.Unsafe
 {
     /// <summary>
-    /// <para>
-    /// Represents the byte array extensions.
-    /// </para>
-    /// <para></para>
+    /// <para>Represents the byte array extensions.</para><para>Представляет расширения байтового массива.</para>
     /// </summary>
     public unsafe static class ByteArrayExtensions
     {
         /// <summary>
-        /// <para>
-        /// Returns the structure using the specified bytes.
-        /// </para>
-        /// <para></para>
+        /// <para>Returns the structure using the specified bytes.</para>
+        /// <para>Возвращает структуру, использующую указанные байты.</para>
         /// </summary>
-        /// <typeparam name="TStruct">
-        /// <para>The struct.</para>
-        /// <para></para>
-        /// </typeparam>
-        /// <param name="bytes">
-        /// <para>The bytes.</para>
-        /// <para></para>
-        /// </param>
-        /// <returns>
-        /// <para>The structure.</para>
-        /// <para></para>
-        /// </returns>
+        /// <typeparam name="TStruct"><para>Type - structure.</para><para>Тип - структура.</para></typeparam>
+        /// <param name="bytes"><para>The bytes.</para><para>Байты.</para></param>
+        /// <param name="nameof(bytes)"><para>String literal.</para><para>Строковый литерал.</para></param>
+        /// <param name="structure"><para>The structure.</para><para>Структура.</para></param>
+        /// <returns><para>The structure.</para><para>Стурктура.</para></returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static TStruct ToStructure<TStruct>(this byte[] bytes)
             where TStruct : struct
@@ -46,6 +34,10 @@ namespace Platform.Unsafe
             }
             return structure;
         }
+        /// <summary>
+        /// <para>Checks whether the argument meets the criteria.</para><para>Проверяет, соответствует ли аргумент критериям.</para>
+        /// </summary>
+        /// <returns><para>Returns true if array length equal to a size of structure instance of TStruct type and false otherwise.</para><para>Возвращает true, если длина массива равна размеру экземпляра структуры типа TStruct, и false в противном случае.</para></returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         private static bool HasSameSizeAs<TStruct>(byte[] array) where TStruct : struct => array.Length == Structure<TStruct>.Size;
     }
