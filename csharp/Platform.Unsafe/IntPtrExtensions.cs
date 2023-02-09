ab586574cfe099ffd38160a567023c5102777875
DdeadliestT <93347396+DdeadliestT@users.noreply.github.com>
2023-02-09 22:04:22 +0300

    Update IntPtrExtensions.cs
    Added comments to the code in Russian.

--- csharp/Platform.Unsafe/IntPtrExtensions.cs
+++ csharp/Platform.Unsafe/IntPtrExtensions.cs
@@ -7,60 +7,27 @@ using static System.Runtime.CompilerServices.Unsafe;
 namespace Platform.Unsafe
 {
     /// <summary>
-    /// <para>
-    /// Represents the int ptr extensions.
-    /// </para>
-    /// <para></para>
+    /// <para>Represents the IntPtr extensions.</para><para>Представляет расширения IntPtr.</para>
     /// </summary>
     public unsafe static class IntPtrExtensions
     {
         /// <summary>
-        /// <para>
-        /// Writes the element value using the specified pointer.
-        /// </para>
-        /// <para></para>
+        /// <para>Writes the element value using the specified pointer.</para><para>Записывает значение элемента, используя заданный указатель.</para>
         /// </summary>
-        /// <typeparam name="TValue">
-        /// <para>The value.</para>
-        /// <para></para>
-        /// </typeparam>
-        /// <param name="pointer">
-        /// <para>The pointer.</para>
-        /// <para></para>
-        /// </param>
-        /// <param name="index">
-        /// <para>The index.</para>
-        /// <para></para>
-        /// </param>
-        /// <param name="value">
-        /// <para>The value.</para>
-        /// <para></para>
-        /// </param>
+        /// <typeparam name="TValue"><para>The value type.</para><para>Тип элемента.</para></typeparam>
+        /// <param name="pointer"><para>The pointer.</para><para>Указатель.</para></param>
+        /// <param name="index"><para>The index.</para><para>Индекс.</para></param>
+        /// <param name="value"><para>The value.</para><para>Значение элемента.</para></param>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void WriteElementValue<TValue>(this IntPtr pointer, long index, TValue value) => Write((byte*)pointer + (SizeOf<TValue>() * index), value);
 
         /// <summary>
-        /// <para>
-        /// Reads the element value using the specified pointer.
-        /// </para>
-        /// <para></para>
+        /// <para>Reads the element value using the specified pointer.</para><para>Считывает значение элемента, используя данный указатель.</para>
         /// </summary>
-        /// <typeparam name="TValue">
-        /// <para>The value.</para>
-        /// <para></para>
-        /// </typeparam>
-        /// <param name="pointer">
-        /// <para>The pointer.</para>
-        /// <para></para>
-        /// </param>
-        /// <param name="index">
-        /// <para>The index.</para>
-        /// <para></para>
-        /// </param>
-        /// <returns>
-        /// <para>The value</para>
-        /// <para></para>
-        /// </returns>
+        /// <typeparam name="TValue"><para>The value type.</para><para>Тип элемента.</para></typeparam>
+        /// <param name="pointer"><para>The pointer.</para><para>Указатель.</para></param>
+        /// <param name="index"><para>The index.</para><para>Индекс.</para></param>
+        /// <returns><para>The value.</para><para>Значение элемента.</para></returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static TValue ReadElementValue<TValue>(this IntPtr pointer, long index) => Read<TValue>((byte*)pointer + (SizeOf<TValue>() * index));
     }
