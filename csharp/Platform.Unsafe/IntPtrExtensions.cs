f7078d7701445d17107ff7f376a5bfa69fae8d1b
DdeadliestT <93347396+DdeadliestT@users.noreply.github.com>
2023-02-12 11:44:00 +0300

    Update IntPtrExtensions.cs

--- csharp/Platform.Unsafe/IntPtrExtensions.cs
+++ csharp/Platform.Unsafe/IntPtrExtensions.cs
@@ -16,7 +16,7 @@ namespace Platform.Unsafe
         /// <para>Writes the element's <paramref name="value"/> at a position defined by the specified <paramref name="index"/> relative to the specified <paramref name="pointer"/>.</para>
         /// <para>Записывает <paramref name="value"/> элемента в позицию определяемую указанным <paramref name="index"/> относительно указанного <paramref name="pointer"/>.</para>
         /// </summary>
-        /// <typeparam name="TValue"><para>The element`s value type.</para><para>Тип значения элемента.</para></typeparam>
+        /// <typeparam name="TValue"><para>The element's value type.</para><para>Тип значения элемента.</para></typeparam>
         /// <param name="pointer"><para>The pointer.</para><para>Указатель.</para></param>
         /// <param name="index">
         /// <para>The ordinal number of an element of type <typeparamref name="TValue"/> that is used as an offset relative to <paramref name="pointer"/>. Elements are counted from zero.</para>
@@ -27,16 +27,16 @@ namespace Platform.Unsafe
         public static void WriteElementValue<TValue>(this IntPtr pointer, long index, TValue value) => Write((byte*)pointer + (SizeOf<TValue>() * index), value);
 
         /// <summary>
-        /// <para>Reads the element`s <paramref name="value"/> at a position defined by the specified <paramref name="index"/> relative to the specified <paramref name="pointer"/>.</para>
-        /// <para>Считывает <paramref name="value"/> элемента в позицию определяемую указанным <paramref name="index"/> относительно указанного <paramref name="pointer"/>.</para>
+        /// <para>Reads the element's <paramref name="value"/> from a position defined by the specified <paramref name="index"/> relative to the specified <paramref name="pointer"/>.</para>
+        /// <para>Считывает <paramref name="value"/> элемента из позиции определяемой указанным <paramref name="index"/> относительно указанного <paramref name="pointer"/>.</para>
         /// </summary>
-        /// <typeparam name="TValue"><para>The element`s value type.</para><para>Тип значение элемента.</para></typeparam>
+        /// <typeparam name="TValue"><para>The element's value type.</para><para>Тип значение элемента.</para></typeparam>
         /// <param name="pointer"><para>The pointer.</para><para>Указатель.</para></param>
         /// <param name="index">
         /// <para>The ordinal number of an element of type <typeparamref name="TValue"/> that is used as an offset relative to <paramref name="pointer"/>. Elements are counted from zero.</para>
         /// <para>Порядковый номер элемента типа <typeparamref name="TValue"/>, который используется в качестве смещения относительно <paramref name="pointer"/>. Отсчёт элементов начинается с нуля.</para>
         /// </param>
-        /// <returns><para>The eleent`s value.</para><para>Значение элемента.</para></returns>
+        /// <returns><para>The element's value.</para><para>Значение элемента.</para></returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static TValue ReadElementValue<TValue>(this IntPtr pointer, long index) => Read<TValue>((byte*)pointer + (SizeOf<TValue>() * index));
     }
