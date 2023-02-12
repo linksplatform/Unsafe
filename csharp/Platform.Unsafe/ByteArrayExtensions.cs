3d2118034f025390c2b84a08740ca4184f7bd40a
DdeadliestT <93347396+DdeadliestT@users.noreply.github.com>
2023-02-12 13:13:48 +0300

    Update ByteArrayExtensions.cs

--- csharp/Platform.Unsafe/ByteArrayExtensions.cs
+++ csharp/Platform.Unsafe/ByteArrayExtensions.cs
@@ -8,20 +8,20 @@ using static System.Runtime.CompilerServices.Unsafe;
 namespace Platform.Unsafe
 {
     /// <summary>
-    /// <para>Represents an extension of an array of bytes of type <typeparamref name="TStruct"/>.</para>
-    /// <para>Представляет расширения массивa байтов типа <typeparamref name="TStruct"/>.</para>
+    /// <para>Represents a set of extension methods for byte arrays.</para>
+    /// <para>Представляет набор методов расширения массивов байт.</para>
     /// </summary>
     public unsafe static class ByteArrayExtensions
     {
         /// <summary>
-        /// <para>Returns <paramref name="structure"/> when pinned <paramref name="pointer"/> equal to the specified <paramref name="bytes"/> so that it does not move when copying.</para>
-        /// <para>Возвращает <paramref name="structure"/> при закреплённом <paramref name="pointer"/> равный указанным <paramref name="bytes"/>, чтобы не он перемещался при копировании.</para>
+        /// <para>Returns <paramref name="structure"/> if the length of the byte array with the specified <paramref name="bytes"/> is equal to the size of the structure.</para>
+        /// <para>Возвращает <paramref name="structure"/>, если длина байтового массива при указанных <paramref name="bytes"/> равна размеру структуры.</para>
         /// </summary>
-        /// <typeparam name="TStruct"><para>The element`s structure type.</para><para>Тип структуры элемента.</para></typeparam>
+        /// <typeparam name="TStruct"><para>The element's structure type.</para><para>Тип структуры элемента.</para></typeparam>
         /// <param name="bytes">
         /// <para>An array of bytes of type <typeparamref name="TStruct"/>.</para>
-        /// <para>Массив байтов типа <typeparamref name="TStruct"/>.</para></param>
-        /// <param name="structure"><para>An array of bytes of the type <typeparamref name="TStruct"/>.</para><para>Массив байтов типа <typeparamref name="TStruct"/>.</para></param>
+        /// <para>Массив байтов типа <typeparamref name="TStruct"/>.</para>
+        /// </param>
         /// <returns><para>The <paramref name="structure"/>.</para></returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static TStruct ToStructure<TStruct>(this byte[] bytes)
@@ -36,7 +36,7 @@ namespace Platform.Unsafe
             }
             return structure;
         }
-        
+
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         private static bool HasSameSizeAs<TStruct>(byte[] array) where TStruct : struct => array.Length == Structure<TStruct>.Size;
     }
