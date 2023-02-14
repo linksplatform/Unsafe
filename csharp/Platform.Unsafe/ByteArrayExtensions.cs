731bc3ce53c54458e7206ab5a6b8af87c22d85df
DdeadliestT <93347396+DdeadliestT@users.noreply.github.com>
2023-02-14 17:57:52 +0300

    Update ByteArrayExtensions.cs

--- csharp/Platform.Unsafe/ByteArrayExtensions.cs
+++ csharp/Platform.Unsafe/ByteArrayExtensions.cs
@@ -11,8 +11,8 @@ namespace Platform.Unsafe
     /// <para>Represents a set of extension methods for byte arrays.</para>
     /// <para>Представляет набор методов расширения массивов байт.</para>
     /// </summary>
-    public unsafe static class ByteArrayExtensions
-    {
+    public unsafe static class ByteArrayExtensions
+    {
         /// <summary>
         /// <para>Returns <paramref name="structure"/> if the length of the byte array with the specified <paramref name="bytes"/> is equal to the size of the structure.</para>
         /// <para>Возвращает <paramref name="structure"/>, если длина байтового массива при указанных <paramref name="bytes"/> равна размеру структуры.</para>
@@ -31,21 +31,21 @@ namespace Platform.Unsafe
         /// <para>Массив байтов, который будет преобразован в тип <typeparamref name="TStruct"/>.</para>
         /// </param>
         /// <returns><para>The structure.</para><para>Структуру.</para></returns>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static TStruct ToStructure<TStruct>(this byte[] bytes)
-            where TStruct : struct
-        {
-            Ensure.OnDebug.ArgumentNotEmpty(bytes, nameof(bytes));
-            Ensure.OnDebug.ArgumentMeetsCriteria(bytes, HasSameSizeAs<TStruct>, nameof(bytes), "Bytes array should be the same length as struct size.");
-            TStruct structure = default;
-            fixed (byte* pointer = bytes)
-            {
-                Copy(ref structure, pointer);
-            }
-            return structure;
-        } 
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        private static bool HasSameSizeAs<TStruct>(byte[] array) where TStruct : struct => array.Length == Structure<TStruct>.Size;
-    }
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static TStruct ToStructure<TStruct>(this byte[] bytes)
+            where TStruct : struct
+        {
+            Ensure.OnDebug.ArgumentNotEmpty(bytes, nameof(bytes));
+            Ensure.OnDebug.ArgumentMeetsCriteria(bytes, HasSameSizeAs<TStruct>, nameof(bytes), "Bytes array should be the same length as struct size.");
+            TStruct structure = default;
+            fixed (byte* pointer = bytes)
+            {
+                Copy(ref structure, pointer);
+            }
+            return structure;
+        }
+        
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static bool HasSameSizeAs<TStruct>(byte[] array) where TStruct : struct => array.Length == Structure<TStruct>.Size;
+    }
 }
